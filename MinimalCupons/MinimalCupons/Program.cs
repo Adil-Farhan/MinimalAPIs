0e798f2a3c75b8387961339d85a1edb206a6ac0d
Adil Farhan <adilfarhan28@gmail.com>
2024-11-20 17:50:45 +0500

    response added

--- MinimalCupons/MinimalCupons/Program.cs
+++ MinimalCupons/MinimalCupons/Program.cs
@@ -5,6 +5,8 @@ using MinimalCupons;
 using MinimalCupons.Data;
 using MinimalCupons.DTO;
 using MinimalCupons.Models;
+using MinimalCupons.Response;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,50 +32,77 @@ app.MapGet("/helloworld/{id:int}", (int id) =>
     return Results.Ok("HEllo" + id);
 });
 
-app.MapGet("/api/coupon", (ILogger <Program> _logger) =>
+app.MapGet("/api/coupon", (ILogger<Program> _logger) =>
 {
     _logger.Log(LogLevel.Information, "Getting All Coupons");
-    //var result = _mapper.Map<List<CouponCreateDTO>>(CouponStore.couponsList);
-    return Results.Ok(CouponStore.couponsList);
+    APIResponse response = new APIResponse();
 
-}).WithName("GetAllCoupon").Produces<IEnumerable<CouponCreateDTO>>(201);
+    response.Results = CouponStore.couponsList;
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.Created;
+
+    return Results.Ok(response);
+
+}).WithName("GetAllCoupon").Produces<IEnumerable<APIResponse>>(201);
 
 
 app.MapGet("/api/coupon/{id:int}", (int id) =>
 {
-    return Results.Ok(CouponStore.couponsList.FirstOrDefault(u => u.Id == id));
-}).WithName("GetCoupon").Produces<Coupon>(201);
+    APIResponse response = new APIResponse();
+
+    response.Results = CouponStore.couponsList.FirstOrDefault(u => u.Id == id);
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.Created;
+
+    return Results.Ok(response);
+}).WithName("GetCoupon").Produces<APIResponse>(201);
 
 
 
 
 
-app.MapPost("/api/coupon/", async (IValidator<CouponCreateDTO> _validator, IMapper _mapper, ILogger<Program> _logger, [FromBody] CouponCreateDTO obj) =>
+app.MapPost("/api/coupon/", async (IValidator<CouponCreateDTO> _validator
+    , IMapper _mapper
+    , ILogger<Program> _logger
+    , [FromBody] CouponCreateDTO obj) =>
 {
     var validationResult = await _validator.ValidateAsync(obj);
-    
+    APIResponse response = new APIResponse();
 
     _logger.Log(LogLevel.Information, "Creating a Coupon: " + obj.Name);
 
-    if(!validationResult.IsValid)
+    if (!validationResult.IsValid)
     {
-        return Results.BadRequest(validationResult.Errors.FirstOrDefault().ToString());
+        response.Results = new CouponCreateDTO();
+        response.IsSuccess = false;
+        response.StatusCode = HttpStatusCode.BadRequest;
+        response.ErrorMessages.Add(validationResult.Errors.FirstOrDefault().ToString());
+        return Results.BadRequest(response);
+    
     }
 
     if (CouponStore.couponsList.FirstOrDefault(x => x.Name.ToLower().Equals(obj.Name.ToLower())) != null)
     {
-        return Results.BadRequest("Coupon Name already Exists");
+        response.Results = new CouponCreateDTO();
+        response.IsSuccess = false;
+        response.StatusCode = HttpStatusCode.BadRequest;
+        response.ErrorMessages.Add("Coupon Name already Exists");
+        return Results.BadRequest(response);
     }
 
     Coupon coupon = _mapper.Map<Coupon>(obj);
     coupon.Id = CouponStore.couponsList.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
     CouponStore.couponsList.Add(coupon);
 
+    
+    response.Results = coupon;
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.Created;
 
     return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, obj);
 }).WithName("CreateCoupon")
 .Accepts<CouponCreateDTO>("application/json")
-.Produces<Coupon>(201).Produces(400);
+.Produces<APIResponse>(201).Produces(400);
 
 
 
---
+++ MinimalCupons/MinimalCupons/Response/APIResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Net;
+
+namespace MinimalCupons.Response
+{
+    public class APIResponse
+    {
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+        public bool IsSuccess { get; set; }
+        public object Results { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
+    }
+}
